04f59fc161a76b6ecb73b3f70329493d2d87f1bc
Success <Success@DESKTOP-J78Q4BN>
2025-07-07 20:34:58 +0530

    test commited

---
+++ test/src/test/LiableEx.java
@@ -0,0 +1,43 @@
+package test;
+
+public class LiableEx {
+	/*public static void main(String[] args) 
+	{
+		int a=10;
+		System.out.println( a+(a=a++));
+	}*/
+	/*public static void main(String[] args) 
+	{
+		int a = 20;
+		int res = a<20 ? a : 'a'; 
+		System.out.println(res);
+		//System.out.println("Hii"== "Hii");
+	//	System.out.println(true == false); 
+      //  System.out.println('a'>=97);
+       // System.out.println("Hello" != true);	
+    } */
+	
+/*public static void main(String[] args) {
+		
+		int a = 10, b = 20;
+		int res = a>b++ && ++a<b ? b++ : b--;
+		System.out.print(a);
+		System.out.print(b);
+	}*/
+	//public static void main(String[] @args) 
+	//{
+		/*System.out.print(100);
+		System.out.println("Hello");
+		System.out.print();*/
+		//System.out.println("All the best");
+	//}
+	
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		System.out.println('a');	
+	}
+
+
+
+}
